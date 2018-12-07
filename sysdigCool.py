13eb4951f2b772e8f684e3e13bde326c7e089cff
Sam Gabrail <sam@SamMac.local>
2018-12-07 15:01:34 -0500

    fixed html

--- sysdigCool.py
+++ sysdigCool.py
@@ -2,6 +2,6 @@ from bottle import route, run
 
 @route('/sysdig')
 def sysdig():
-return "<h1>Sysdig Rocks CI/CD!</h1><p>What do you think?</p>"
+return "<h1>Sysdig Rocks CI/CD!</h1>"
 
 run(host='0.0.0.0', port=8001, debug=True)
